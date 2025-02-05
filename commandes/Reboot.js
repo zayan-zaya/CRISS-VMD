83b2a88b5fd8d3d4df0ae99fb870acf2a953a4c0
CRISS VEVO <crissvevo@icloud.com>
2025-02-06 02:53:20 +0300

    Update Reboot.js

--- commandes/Reboot.js
+++ commandes/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("ANYWAY-MD bot Restarting ⏳");
+    repondre("CRISS-VMD bot Restarting ⏳");
 
   exec("pm2 restart all");
   
