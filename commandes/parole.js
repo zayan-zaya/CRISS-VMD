95ade234948868aae5999945232ff9238d46c712
CRISS VEVO <crissvevo@icloud.com>
2025-02-06 03:41:31 +0300

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------Anyway-lyrics-finder--------
+    let msg = `---------Criss-lyrics-finder--------
 
 * *Artist :* ${lyrics.artist}
 
