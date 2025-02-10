81af62f9978d1cf88676c54d93170d931e0309a0
CRISS VEVO <crissvevo@icloud.com>
2025-02-10 10:34:41 +0300

    Update zspotify.js

--- commandes/zspotify.js
+++ commandes/zspotify.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 ezra({
   nomCom: "spotifylist",
   aliases: ["spotifysearch", "splaylist"],
-  categorie: "Fledi-search",
+  categorie: "Criss-search",
   reaction: "🎬"
 }, async (dest, zk, commandeOptions) => {
   const { repondre, arg } = commandeOptions;
@@ -25,7 +25,7 @@ ezra({
     }
 
     // Construct playlist message
-    let playlistMessage = `LUCKY PLANET SPOTIFY PLAY\n\n`;
+    let playlistMessage = `CRISS VEVO SPOTIFY PLAY\n\n`;
 
     // Loop through search results and construct track info with numbers
     searchData.forEach((track, index) => {
@@ -46,9 +46,9 @@ ezra({
           mentionedJid: [dest],  // Mention the sender's JID
           externalAdReply: {
             showAdAttribution: true,
-            title: "LUCKY MD SPOTIFY PLAY",
-            body: "Credit by frediezra",
-            sourceUrl: "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
+            title: "CRISS VEVO SPOTIFY PLAY",
+            body: "Credit by CRISS VEVO",
+            sourceUrl: "https://whatsapp.com/channel/0029Vb0HIV2G3R3s2II4181g",
             mediaType: 1,
             renderLargerThumbnail: false,
           },
