7f4cd5f59024125afcaf33b9f75f2fd71d56788b
CRISS VEVO <crissvevo@icloud.com>
2025-02-22 23:36:19 +0300

    Update alive.js

--- alive.js
+++ alive.js
@@ -71,7 +71,7 @@ else {
 }
 
     } else {
-        if(!superUser) { repondre("there is no alive for this bot") ; return};
+        if(!superUser) { repondre("CRISS VEVO BOT IS ALiVE ALL THE TIME👨‍💻") ; return};
 
       await   repondre("You have not yet saved your alive, to do this;  enter after alive your message and your image or video link in this context: .alive message;lien");
          repondre("don't do fake thinks :)")
