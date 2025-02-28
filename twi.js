5d76d3d946787a232484d3b304f38a6a483e2d85
CRISS VEVO <crissvevo@icloud.com>
2025-03-01 02:07:24 +0300

    Update twi.js

--- twi.js
+++ twi.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 zokou({
   nomCom: "twittersearch",
   aliases: ["xsearch", "twitterlist", "tweetsearch", "xsearch"],
-  categorie: "Dullah-search",
+  categorie: "Criss-search",
   reaction: "📽️"
 }, async (dest, zk, commandeOptions) => {
   const { repondre, arg } = commandeOptions;
@@ -26,7 +26,7 @@ zokou({
     }
 
     // Construct the search message
-    let searchMessage = `DULLAH XMD PLANET TWITTER SEARCH\n\n`;
+    let searchMessage = `CRISS VMD PLANET TWITTER SEARCH\n\n`;
     searchMessage += `Creator: ${response.data.creator}\n\n`;  // Include the creator info
 
     // Loop through search results and append details to the message
@@ -48,9 +48,9 @@ zokou({
           mentionedJid: [dest],
           externalAdReply: {
             showAdAttribution: true,
-            title: "DULLAH XMD TWITTER SEARCH",
-            body: "DULLAH XMD V2 IS FIRE",
-            sourceUrl: "https://chat.whatsapp.com/IdRXU9UcO8K50GPelOyhxh",
+            title: "CRISS VMD TWITTER SEARCH",
+            body: "CRISS VMD IS FIRE",
+            sourceUrl: "https://whatsapp.com/channel/0029Vb0HIV2G3R3s2II4181g",
             mediaType: 1,
             renderLargerThumbnail: false,
           },
