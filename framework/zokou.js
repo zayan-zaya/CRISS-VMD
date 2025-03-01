0a0111dc3da7384929e9392b45fd1ae4996ad7fd
CRISS VEVO <crissvevo@icloud.com>
2025-03-02 02:09:55 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "👨‍💻";
+        infoComs.reaction = "💫";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
