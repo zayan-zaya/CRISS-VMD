d87b75e86fff26760be50e184f96b9bb8bf9ee27
CRISS VEVO <crissvevo@icloud.com>
2025-03-01 15:58:38 +0300

    Update chanel1.js

--- commandes/chanel1.js
+++ commandes/chanel1.js
@@ -5,7 +5,7 @@ const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Salut je m\'appelle *CRISS-VMD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *DULLAH*';
+    let d = ' developpé par *CRISS*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029Vb0HIV2G3R3s2II4181g';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
