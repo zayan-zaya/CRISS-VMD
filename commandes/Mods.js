b9547765f91b126b8381e670b3971472c7ed99c7
CRISS VEVO <crissvevo@icloud.com>
2025-03-02 00:34:31 +0300

    Add files via upload

--- commandes/Mods.js
+++ commandes/Mods.js
@@ -70,7 +70,7 @@ const sleep =  (ms) =>{
   
           const sticker = new Sticker(buffer.data, {
             pack: nomAuteurMessage,
-            author: "Beltah-md",
+            author: "Anyway-md",
             type: StickerTypes.FULL,
             categories: ['🤩', '🎉'],
             id: '12345',
@@ -435,89 +435,6 @@ if (!superUser) {repondre('This command is only allowed to the bot owner') ; ret
 
 
 zokou({ nomCom: "save", categorie: "Mods" }, async (dest, zk, commandeOptions) => {
-
-  const { repondre , msgRepondu , superUser, auteurMessage } = commandeOptions;
-  
-    if ( superUser) { 
-  
-      if(msgRepondu) {
-
-        console.log(msgRepondu) ;
-
-        let msg ;
-  
-        if (msgRepondu.imageMessage) {
-  
-          
-  
-       let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.imageMessage) ;
-       // console.log(msgRepondu) ;
-       msg = {
-  
-         image : { url : media } ,
-         caption : msgRepondu.imageMessage.caption,
-         
-       }
-      
-  
-        } else if (msgRepondu.videoMessage) {
-  
-          let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.videoMessage) ;
-  
-          msg = {
-  
-            video : { url : media } ,
-            caption : msgRepondu.videoMessage.caption,
-            
-          }
-  
-        } else if (msgRepondu.audioMessage) {
-      
-          let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.audioMessage) ;
-         
-          msg = {
-     
-            audio : { url : media } ,
-            mimetype:'audio/mp4',
-             }     
-          
-        } else if (msgRepondu.stickerMessage) {
-  
-      
-          let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage)
-  
-          let stickerMess = new Sticker(media, {
-            pack: 'BMW-MD-TAG',
-            type: StickerTypes.CROPPED,
-            categories: ["🤩", "🎉"],
-            id: "12345",
-            quality: 70,
-            background: "transparent",
-          });
-          const stickerBuffer2 = await stickerMess.toBuffer();
-         
-          msg = { sticker: stickerBuffer2}
-  
-  
-        }  else {
-            msg = {
-               text : msgRepondu.conversation,
-            }
-        }
-  
-      zk.sendMessage(auteurMessage,msg)
-  
-      } else { repondre('Mention the message that you want to save') }
-  
-  } else {
-    repondre('only mods can use this command')
-  }
-  
-
-  })
-;
-
-zokou({ nomCom: "vv", categorie: "Mods" }, async (dest, zk, commandeOptions) => {
 
   const { repondre , msgRepondu , superUser, auteurMessage } = commandeOptions;
   
