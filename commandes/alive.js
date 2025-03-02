a4b9066c75d91ba5189cf9722a7b4fc60b8ddd31
CRISS VEVO <crissvevo@icloud.com>
2025-03-03 00:46:18 +0300

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -71,9 +71,53 @@ else {
 }
 
     } else {
-        if(!superUser) { repondre("CRISS VMD BOT IS ALiVE ALL THE TIME👨‍💻") ; return};
+        const audioFiles = [
+    'https://files.catbox.moe/hpwsi2.mp3',
+    'https://files.catbox.moe/xci982.mp3',
+    'https://files.catbox.moe/utbujd.mp3',
+    'https://files.catbox.moe/w2j17k.m4a',
+    'https://files.catbox.moe/851skv.m4a',
+    'https://files.catbox.moe/qnhtbu.m4a',
+    'https://files.catbox.moe/lb0x7w.mp3',
+    'https://files.catbox.moe/efmcxm.mp3',
+    'https://files.catbox.moe/gco5bq.mp3',
+    'https://files.catbox.moe/26oeeh.mp3',
+    'https://files.catbox.moe/a1sh4u.mp3',
+    'https://files.catbox.moe/vuuvwn.m4a',
+    'https://files.catbox.moe/wx8q6h.mp3',
+    'https://files.catbox.moe/uj8fps.m4a',
+    'https://files.catbox.moe/dc88bx.m4a',
+    'https://files.catbox.moe/jy8ee7.mp3'
+    'https://files.catbox.moe/9j7rh2.mp3'
+  ];
 
-      await   repondre("CRISS VMD BOT IS ALiVE ALL THE TIME👨‍💻");
+  // Randomly pick an audio file from the list
+  const selectedAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+
+  // Audio message object
+  const audioMessage = {
+    audio: {
+      url: selectedAudio,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,  // Marking this as a "Push-to-Talk" message
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'shizo',
+    contextInfo: {
+      externalAdReply: {
+        title: '𝐂𝐑𝐈𝐒𝐒 𝐌𝐃 𝐈𝐒 𝐀𝐋𝐈𝐕𝐄 𝐀𝐋𝐋 𝐓𝐇𝐄 𝐓𝐈𝐌𝐄',
+        body: conf.OWNER_NAME,
+        thumbnailUrl: conf.URL,
+        sourceUrl: conf.GURL, // Corrected variable name
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  };
+
+  // Send the audio message with the context of the original message
+  await zk.sendMessage(dest, audioMessage, { quoted: ms });
+})
      }
  } else {
 
