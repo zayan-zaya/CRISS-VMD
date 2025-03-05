b8d78e8a1b3e0d439cca543d3a7c8a55b27bbc74
CRISS VEVO <crissvevo@icloud.com>
2025-03-05 17:26:24 +0300

    Update bongo.js

--- commandes/bongo.js
+++ commandes/bongo.js
@@ -56,7 +56,7 @@ zokou({
   } = _0x53dc51;
   const _0x2784cf = await fetch('https://favqs.com/api/qotd');
   const _0x1f25fb = await _0x2784cf.json();
-  const _0x2b238b = "\nCTISS MD QUOTE MESSAGE \n◇ _" + _0x1f25fb.quote.body + "_\n\n\n◇ *AUTHOR:* " + _0x1f25fb.quote.author + "\n\n\n\n\n◇ *©Criss Vevo*\n\n\n╔═════◇\n║◇ *CRISS MD*\n╚════════════════> ";
+  const _0x2b238b = "\nCRISS MD QUOTE MESSAGE \n◇ _" + _0x1f25fb.quote.body + "_\n\n\n◇ *AUTHOR:* " + _0x1f25fb.quote.author + "\n\n\n\n\n◇ *©Criss Vevo*\n\n\n╔═════◇\n║◇ *CRISS MD*\n╚════════════════> ";
   _0x3bef2d(_0x2b238b);
 });
 zokou({
