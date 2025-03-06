0b82f1591a3512699230f485b8775a6245e9cb43
CRISS VEVO <crissvevo@icloud.com>
2025-03-06 13:11:41 +0300

    Update play.js

--- commandes/play.js
+++ commandes/play.js
@@ -8,7 +8,7 @@ zokou({
   nomCom: "play",
   aliases: ["song", "playdoc", "audio", "mp3"],
   categorie: "Search",
-  reaction: "🎥"
+  reaction: "🎙️"
 }, async (dest, zk, commandOptions) => {
   const { arg, ms, repondre } = commandOptions;
 
